f5082c1c4bc65f7e548340e1ea1ac33bcbfdf897
SlimTim10 <slimtim10@gmail.com>
2015-09-02 17:08:37 -0400

    First commit

---
+++ facebook-album-catalog.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Facebook Album Catalog
+Plugin URI:  http://none.com
+Description: Display a catalog that is dynamically linked to a Facebook album.
+Version:     1.0
+Author:      SlimTim10
+Author URI:  https://github.com/SlimTim10
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
